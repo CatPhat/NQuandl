4ddc300c12f61aebd6d1ef2ed80daeee2a940c05
CatPhat <kian@msoon.com>
2015-04-14 16:35:13 -0700

    add correct nquandl.queue folder name and content

---
+++ nquandl.queue/CompositionRoot/CompositionRoot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using NQuandl.Client;
+using SimpleInjector;
+using SimpleInjector.Extensions;
+
+namespace NQuandl.Queue
+{
+    public static class CompositionRoot
+    {
+        public static void Configure()
+        {
+            var container = new Container();
+            container.ComposeRoot();
+        }
+
+        public static void ComposeRoot(this Container container)
+        {
+            container.Register<IServiceProvider>(() => container, Lifestyle.Singleton);
+            container.Register<IQuandlService>(() => new QuandlService());
+            container.RegisterOpenGeneric(typeof(IQuandlRequestQueue<>), typeof(QuandlRequestQueue<>));
+            container.RegisterSingle<IDownloadQueue>(() => new DownloadQueue());
+        
+        
+       
+        }
+    }
+}
