bdfee00c68d7ecd921a1e44355c420d16f375974
Kian <kgmpro@gmail.com>
2015-04-17 16:43:03 -0700

    Add Request String

---
+++ nquandl.client/models/QuandlRequests/RequestString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NQuandl.Client.Models.QuandlRequests
+{
+    public class RequestString : BaseQuandlRequestV1<RequestStringResponse>
+    {
+        private readonly string _queryCode;
+        private readonly string _parameters;
+        public RequestString(string querycode, string parameters)
+        {
+            _queryCode = querycode;
+            _parameters = parameters;
+        }
+
+        public override string QueryCode
+        {
+            get { return _queryCode; }
+        }
+
+        public override string Parameters
+        {
+            get { return _parameters; }
+        }
+    }
+
+    public class RequestStringResponse : QuandlResponse
+    {
+        public string String { get; set; }
+    }
+}
