2072f20d29776ce02a46a0afa70cb8b8d7f340a2
CatPhat <kian@msoon.com>
2015-04-17 14:15:30 -0700

    Change status format

--- nquandl.client/Program.cs
+++ nquandl.client/Program.cs
@@ -51,10 +51,11 @@ namespace NQuandl.TestConsole
             Console.WriteLine("                     Total: {0}", NQueue.GetQueueStatus().TotalRequests);
             Console.WriteLine("                 Remaining: {0}", NQueue.GetQueueStatus().RequestsRemaining);
             Console.WriteLine("                 Processed: {0}", NQueue.GetQueueStatus().RequestsProcessed);
-            Console.WriteLine("            Time Remaining: {0} sec", NQueue.GetQueueStatus().TimeRemaining);
+            Console.WriteLine("              Time Elapsed: {0}", NQueue.GetQueueStatus().TimeElapsed);
+            Console.WriteLine("            Time Remaining: {0}", NQueue.GetQueueStatus().TimeRemaining);
             Console.WriteLine("            RequestsPerSec: {0} ", NQueue.GetQueueStatus().RequestsPerSecond);
             Console.WriteLine("10 Minutes at Current Rate: {0} ", NQueue.GetQueueStatus().HowManyRequestsIn10MinutesAtCurrentRate);
-            Console.WriteLine("Last Response: {0} \n", NQueue.GetQueueStatus().LastResponse);
+            Console.WriteLine("\nLast Response: {0} \n", NQueue.GetQueueStatus().LastResponse);
             Console.WriteLine("{0}",new string('.', NQueue.GetQueueStatus().RequestsRemaining));
 
         }
