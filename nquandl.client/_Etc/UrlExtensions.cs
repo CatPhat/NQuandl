1ec00cfdf63f58711fe67a39123c9b3a2b76583c
Kian <kgmpro@gmail.com>
2015-04-26 17:21:44 -0700

    Remove unused code

--- nquandl.client/_Etc/UrlExtensions.cs
+++ nquandl.client/_Etc/UrlExtensions.cs
@@ -10,18 +10,6 @@ namespace NQuandl.Client
             return uri + "&auth_token=" + apiKey;
         }
 
-        internal static RequestAttributes GetRequestAttributes<T>(this BaseQuandlRequestWithQueryOptions<T> request) where T : QuandlResponse
-        {
-            var attributes = (RequestAttributes[]) typeof (T).GetCustomAttributes(typeof (RequestAttributes), true);
-            if (attributes.Length <= 0) return null;
-            var requestAttributes = new RequestAttributes
-            {
-                DatabaseCode = attributes[0].DatabaseCode,
-                TableCode = attributes[1].TableCode
-            };
-            return requestAttributes;
-        }
-
     }
 }
 
