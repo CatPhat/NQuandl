886e890cf83b0f749ae090980e7cc547f589eac8
CatPhat <kian@msoon.com>
2015-04-30 16:52:56 -0700

    fix base response

--- nquandl.client/Models/Base/BaseResponse.cs
+++ nquandl.client/Models/Base/BaseResponse.cs
@@ -28,7 +28,7 @@ namespace NQuandl.Client.Models
         }
     }
 
-    [Serializable]
+    [DataContract]
     public class QuandlV1Response : QuandlResponse
     {
         public QuandlV1Response(QuandlCode quandlCode) : base(quandlCode)
@@ -36,55 +36,55 @@ namespace NQuandl.Client.Models
         }
 
         [DataMember(Name = "errors")]
-        public Dictionary<string, string> Errors { get; internal set; }
+        public Dictionary<string, string> Errors { get;  set; }
 
         [DataMember(Name = "id")]
-        public int MetadataId { get; internal set; }
+        public int MetadataId { get;  set; }
 
         [DataMember(Name = "source_name")]
-        public string SourceName { get; internal set; }
+        public string SourceName { get;  set; }
 
         [DataMember(Name = "source_code")]
-        public string SourceCode { get; internal set; }
+        public string SourceCode { get;  set; }
 
         [DataMember(Name = "code")]
-        public string Code { get; internal set; }
+        public string Code { get;  set; }
 
         [DataMember(Name = "name")]
-        public string Name { get; internal set; }
+        public string Name { get;  set; }
 
         [DataMember(Name = "urlize_name")]
-        public string UrlizeName { get; internal set; }
+        public string UrlizeName { get;  set; }
 
         [DataMember(Name = "display_url")]
-        public string DisplayUrl { get; internal set; }
+        public string DisplayUrl { get;  set; }
 
         [DataMember(Name = "description")]
-        public string Descrption { get; internal set; }
+        public string Descrption { get;  set; }
 
         [DataMember(Name = "updated_at")]
-        public DateTime UpdatedAt { get; internal set; }
+        public DateTime UpdatedAt { get;  set; }
 
         [DataMember(Name = "frequency")]
-        public string Frequency { get; internal set; }
+        public string Frequency { get;  set; }
 
         [DataMember(Name = "from_date")]
-        public string FromDate { get; internal set; }
+        public string FromDate { get;  set; }
 
         [DataMember(Name = "to_date")]
-        public string ToDate { get; internal set; }
+        public string ToDate { get;  set; }
 
         [DataMember(Name = "column_names")]
-        public string[] ColumnNames { get; internal set; }
+        public string[] ColumnNames { get;  set; }
 
         [DataMember(Name = "_private")]
-        public bool Private { get; internal set; }
+        public bool Private { get;  set; }
 
         [DataMember(Name = "type")]
-        public object Type { get; internal set; }
+        public object Type { get;  set; }
 
         [DataMember(Name = "premium")]
-        public bool Premium { get; internal set; }
+        public bool Premium { get;  set; }
 
         [DataMember(Name = "data")]
         public object[][] Data { get; set; }
