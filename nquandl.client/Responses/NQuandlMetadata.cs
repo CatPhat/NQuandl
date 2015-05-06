3959378bc5f04d5e67d1a2d5b481c4390eefba1a
Kian <kgmpro@gmail.com>
2015-05-05 20:03:32 -0700

    Add NQuandlMetadata class

---
+++ nquandl.client/Responses/NQuandlMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace NQuandl.Client.Responses
+{
+    public class NQuandlMetadata
+    {
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+        public DateTime RequestStartTime { get; set; }
+        public DateTime RequestEndTime { get; set; }
+        
+        public TimeSpan RequestTimeSpan
+        {
+            get { return RequestStartTime - RequestEndTime; }
+        }
+    }
+}
