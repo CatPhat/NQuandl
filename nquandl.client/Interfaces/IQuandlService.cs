f498c84ca7e8904e29dbe77b9cb131fc88d37bf9
Kian <kgmpro@gmail.com>
2015-05-05 20:12:27 -0700

    fix to match class

--- nquandl.client/Interfaces/IQuandlService.cs
+++ nquandl.client/Interfaces/IQuandlService.cs
@@ -1,11 +1,9 @@
 ﻿using System.Threading.Tasks;
-using NQuandl.Client.Responses;
 
 namespace NQuandl.Client.Interfaces
 {
-    //public interface IQuandlService
-    //{
-    //    Task<TResponse> GetAsync<TResponse>(IReturn<TResponse> request) where TResponse : QuandlResponse;
-    //    Task<string> GetStringAsync(string url);
-    //}
+    public interface IQuandlService
+    {
+        Task<string> GetAsync(IQuandlRequest request);
+    }
 }
