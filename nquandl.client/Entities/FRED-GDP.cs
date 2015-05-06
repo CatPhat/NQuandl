a99687095554cd6cf8084ef8341ec8eabab605e4
Kian <kgmpro@gmail.com>
2015-05-05 19:55:15 -0700

    clean up

--- nquandl.client/Entities/FRED-GDP.cs
+++ nquandl.client/Entities/FRED-GDP.cs
@@ -1,6 +1,5 @@
-﻿using NQuandl.Client.Interfaces;
-using NQuandl.Client.Requests;
-using NQuandl.Client.Responses;
+﻿using NQuandl.Client.Entities.Base;
+using NQuandl.Client.Interfaces;
 
 namespace NQuandl.Client.Entities
 {
@@ -20,19 +19,6 @@ namespace NQuandl.Client.Entities
         }
     }
 
-    public class Request_FRED_GDP : NQuandlRequest<FRED_GDP, QuandlResponseV1>
-    {
-        public override IMapData<FRED_GDP> Mapper
-        {
-            get { return new Map_FRED_GDP(); }
-        }
-
-        public override IReturn<QuandlResponseV1> QuandlRequest
-        {
-            get { return new QuandlRequestV1(Parameters); }
-        }
-    }
-
     public class Map_FRED_GDP : IMapData<FRED_GDP>
     {
         public FRED_GDP Map(object[] objects)
@@ -44,6 +30,4 @@ namespace NQuandl.Client.Entities
             };
         }
     }
-
-  
 }
