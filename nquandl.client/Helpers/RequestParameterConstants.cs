46f3098ba401fe0681e6b97ea0ee77c2706d94c8
Kian <kgmpro@gmail.com>
2015-05-05 20:07:50 -0700

    Refactor constants to reflect new enum ResponseFormat

--- nquandl.client/Helpers/RequestParameterConstants.cs
+++ nquandl.client/Helpers/RequestParameterConstants.cs
@@ -2,17 +2,16 @@
 {
     public static class RequestParameterConstants
     {
-        public const string SortOrder = "sort_order=";
-        public const string ExcludeHeaders = "exclude_headers=";
-        public const string Rows = "rows=";
-        public const string TrimStart = "trim_start=";
-        public const string TrimEnd = "trim_end=";
-        public const string Column = "column=";
-        public const string Frequency = "collapse=";
-        public const string Transformation = "transformation=";
-        public const string ExcludeData = "exclude_data=";
-        public const string AuthToken = "auth_token=";
-        public const string JsonFormat = "json";
+        public const string SortOrder = "sort_order";
+        public const string ExcludeHeaders = "exclude_headers";
+        public const string Rows = "rows";
+        public const string TrimStart = "trim_start";
+        public const string TrimEnd = "trim_end";
+        public const string Column = "column";
+        public const string Frequency = "collapse";
+        public const string Transformation = "transformation";
+        public const string ExcludeData = "exclude_data";
+        public const string AuthToken = "auth_token";
         public const string ApiVersion1 = "v1/datasets";
         public const string ApiVersion2 = "v2/datasets";
     }
