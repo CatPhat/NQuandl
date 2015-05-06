124d33cd2f728c7afa79c9d1489c6b95fffdc86f
Kian <kgmpro@gmail.com>
2015-05-05 20:04:57 -0700

    simplify overall function of class

--- nquandl.client/QuandlService.cs
+++ nquandl.client/QuandlService.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using NQuandl.Client.Requests;
+using NQuandl.Client.Interfaces;
 
 namespace NQuandl.Client
 {
     public class QuandlService
     {
-        private readonly string _baseUrl;
+        private readonly Uri _baseUri;
 
         public QuandlService(string baseUrl)
         {
-            _baseUrl = baseUrl;
-        }
-        
-        public async Task<string> GetStringAsync(IQuandlRequest request)
-        {
-            var response = await GetAsync(request);
-            return await response.Content.ReadAsStringAsync();
+            _baseUri = new Uri(baseUrl);
         }
 
-        public async Task<HttpResponseMessage> GetAsync(IQuandlRequest request)
+        public async Task<string> GetStringAsync(IQuandlRequest request)
+        {
+            return await GetAsync(request);
+        }
+
+        public async Task<string> GetAsync(IQuandlRequest request)
         {
             var httpClient = new HttpClient();
-            var uri = new Uri(_baseUrl + request.Uri);
-            return await httpClient.GetAsync(uri);
+            var uri = new Uri(_baseUri + "/" + request.Uri.Uri);
+            return await httpClient.GetStringAsync(uri);
         }
     }
 }
