c344699061153085fb6b07ca443407ce6599a6fd
CatPhat <kian@msoon.com>
2015-05-11 15:44:13 -0700

    add quandl v2 metadata request

---
+++ nquandl.client/Requests/DeserializeMetadataRequestV2.cs
@@ -0,0 +1,21 @@
+﻿using NQuandl.Client.Interfaces;
+using NQuandl.Client.Responses;
+using NQuandl.Client.URIs;
+
+namespace NQuandl.Client.Requests
+{
+    public class DeserializeMetadataRequestV2 : IQuandlJsonRequest<JsonResponseV2>
+    {
+        public readonly ForcedRequestOptionsV2 _options;
+
+        public DeserializeMetadataRequestV2(ForcedRequestOptionsV2 options)
+        {
+            _options = options;
+        }
+
+        public IContainUri Uri
+        {
+            get { return new QuandlJsonUriV2(_options); }
+        }
+    }
+}
