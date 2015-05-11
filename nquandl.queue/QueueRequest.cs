a90f3f74369cb3bf4960f4785b9f868366f329cc
Kian <kgmpro@gmail.com>
2015-05-10 23:35:08 -0700

    Add QueueRequest

---
+++ nquandl.queue/QueueRequest.cs
@@ -0,0 +1,22 @@
+﻿using NQuandl.Client.Entities.Base;
+using NQuandl.Client.Interfaces;
+using NQuandl.Client.Requests;
+
+namespace NQuandl.Queue
+{
+    public class QueueRequest<TEntity> where TEntity : QuandlEntity, new()
+    {
+        public RequestParameterOptions Options { get; set; }
+
+        public DeserializeEntityRequest<TEntity> DeserializeEntityRequest
+        {
+            get
+            {
+                return new DeserializeEntityRequest<TEntity>
+                {
+                    Options = Options
+                };
+            }
+        }
+    }
+}
