1a1199873e2b6ccc23a86e07eacf794bf2990c29
CatPhat <kian@msoon.com>
2015-05-11 15:43:27 -0700

    test real quandl queue metadata download

--- nquandl.client/Program.cs
+++ nquandl.client/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using NQuandl.Client;
-using NQuandl.Client.Helpers;
 using NQuandl.Client.Requests;
+using NQuandl.Queue;
 
 namespace NQuandl.TestConsole
 {
@@ -10,25 +13,47 @@ namespace NQuandl.TestConsole
     {
         private static void Main(string[] args)
         {
-            var options = new RequestOptionsV1
+            var saveToFile = new SaveToFile();
+            var printStatus = new PrintStatus();
+            var task = Task.WhenAll(saveToFile.QueryAndSave());
+            while (task.IsCompleted == false)
             {
-                ApiKey = QuandlServiceConfiguration.ApiKey,
-                ExcludeData = Exclude.True
-
-            };
-            var client = new QuandlService("https://quandl.com/api");
-            var request = new JsonStringRequestV1("FRED/GDP")
-            {
-                Options = options
-            };
-
-            var result = client.GetStringAsync(request).Result;
-            Console.WriteLine(result);
+                printStatus.Print();
+            }
             Console.WriteLine("done");
             Console.ReadLine();
         }
     }
 
+    public class SaveToFile
+    {
+        public async Task<int> QueryAndSave()
+        {
+            var requests = new List<DeserializeMetadataRequestV2>();
+            for (var i = 1; i <= 521; i++)
+            {
+                var options = new ForcedRequestOptionsV2(QuandlServiceConfiguration.ApiKey, "*", "UNDATA", 300, i);
+                var request = new DeserializeMetadataRequestV2(options);
+                requests.Add(request);
+            }
+
+            var tasks = requests.Select(async request =>
+            {
+                var response = await NQueue.GetStringAsync(request);
+                using (
+                    var writer =
+                        new StreamWriter(@"C:\Users\kian.monsoon\Documents\SYSADMIN\DEV\GITHUB\NQuandl\responses\" +
+                                         request._options.SourceCode + request._options.Page + ".json"))
+                {
+                    writer.Write(response);
+                }
+            }).ToList();
+
+            await Task.WhenAll(tasks);
+            return await Task.FromResult(0);
+
+        }
+    }
 
 
     public class QueueTest
@@ -47,7 +72,6 @@ namespace NQuandl.TestConsole
             {
                 printStatus.Print();
             }
-
         }
     }
 }
--- nquandl.console/Verbose.cs
+++ nquandl.console/Verbose.cs
@@ -15,7 +15,7 @@ namespace NQuandl.TestConsole
             Console.WriteLine("            RequestsPerSec: {0} ", NQueue.GetQueueStatus().RequestsPerSecond);
             Console.WriteLine("10 Minutes at Current Rate: {0} ",
                 NQueue.GetQueueStatus().HowManyRequestsIn10MinutesAtCurrentRate);
-            Console.WriteLine(NQueue.GetQueueStatus().LastResponse);
+           // Console.WriteLine(NQueue.GetQueueStatus().LastResponse);
             //Console.WriteLine("{0}",new string('.', NQueue.GetQueueStatus().RequestsRemaining));
         }
     }
