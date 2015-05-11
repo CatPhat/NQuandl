2819789cd14c9abd5987d0441eb5ca9afdc88bcb
Kian <kgmpro@gmail.com>
2015-05-10 23:35:22 -0700

    Add QueueStatusLogger

---
+++ nquandl.queue/QueueStatusLogger.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace NQuandl.Queue
+{
+    public interface IQueueStatusLogger
+    {
+        Task AddUnprocessedCount(int amount);
+        Task AddProcessedCount(int amount);
+        QueueStatus Status { get; }
+    }
+
+    public class QueueStatusLogger : IQueueStatusLogger
+    {
+        private readonly QueueStatus _queueStatus;
+        public QueueStatusLogger()
+        {
+            _queueStatus = new QueueStatus();
+        }
+        
+        public Task AddUnprocessedCount(int amount)
+        {
+            _queueStatus.RequestsUnprocessed = _queueStatus.RequestsUnprocessed + amount;
+            return Task.FromResult(0);
+        }
+
+        public Task AddProcessedCount(int amount)
+        {
+            _queueStatus.RequestsProcessed = _queueStatus.RequestsProcessed + amount;
+            return Task.FromResult(0);
+        }
+
+        public QueueStatus Status
+        {
+            get { return _queueStatus; }
+        }
+    }
+}
