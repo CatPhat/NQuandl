d3748ce53e64ddf1c7a527ebaa98425eb26f4ec8
Kian <kgmpro@gmail.com>
2015-05-24 19:39:49 -0700

    Add test message bus

---
+++ nquandl.queue/Bus/IHandleAsync.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace NQuandl.Queue.Bus
+{
+    public interface IHandleAsync<in TMessage>
+    {
+        Task HandleAsync(TMessage message, CancellationToken cancellationToken);
+    }
+}
