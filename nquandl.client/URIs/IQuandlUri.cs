50631648c315e884998a19656d7173912cad63eb
Kian <kgmpro@gmail.com>
2015-05-24 19:40:48 -0700

    simplify IQuandlUri

---
+++ nquandl.client/URIs/IQuandlUri.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using NQuandl.Client.Requests;
+
+namespace NQuandl.Client.URIs
+{
+    public interface IQuandlUri
+    {
+        string PathSegment { get; }
+        Dictionary<string,string> QueryParmeters { get; }
+    }
+}
