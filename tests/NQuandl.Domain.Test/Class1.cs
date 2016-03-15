5f342584263f1c7821c132b44528ddb1ec6d223c
Kian Movasagi <kian@vedos.io>
2016-03-14 23:24:15 -0700

    Add test project

---
+++ tests/NQuandl.Domain.Test/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+using Xunit;
+
+
+namespace NQuandl.Domain.Test
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Class1
+    {
+   
+        [Fact]
+        public void PassingTest()
+        {
+            const int sum = 2 + 2;
+            Assert.Equal(4, sum);
+        }
+
+        [Fact]
+        public void ShouldFail()
+        {
+            const int sum = 2 + 2;
+            Assert.Equal(0, sum);
+        }
+    }
+}
