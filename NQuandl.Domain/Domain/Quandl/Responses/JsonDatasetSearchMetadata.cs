8fd152915de0cd627a7cb696bc9ff96b32a09d15
Kian Movasagi <kian@vedos.io>
2016-03-29 15:24:33 -0700

    Add JsonSearchDataset responses

---
+++ NQuandl.Domain/Domain/Quandl/Responses/JsonDatasetSearchMetadata.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace NQuandl.Domain.Quandl.Responses
+{
+    public class JsonDatasetSearchMetadata
+    {
+        [JsonProperty("query")]
+        public string Query { get; set; }
+
+        [JsonProperty("per_page")]
+        public int PerPage { get; set; }
+
+        [JsonProperty("current_page")]
+        public int CurrentPage { get; set; }
+
+        [JsonProperty("prev_page")]
+        public object PrevPage { get; set; }
+
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonProperty("total_count")]
+        public int TotalCount { get; set; }
+
+        [JsonProperty("next_page")]
+        public object NextPage { get; set; }
+
+        [JsonProperty("current_first_item")]
+        public int CurrentFirstItem { get; set; }
+
+        [JsonProperty("current_last_item")]
+        public int CurrentLastItem { get; set; }
+    }
+}
---
+++ NQuandl.Domain/Domain/Quandl/Responses/JsonResultDatasetSearch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NQuandl.Domain.Quandl.Responses
+{
+    
+    public class JsonResultDatasetSearch
+    {
+        [JsonProperty("datasets")]
+        public JsonDatasetSearchDataset[] JsonSearchDatasets { get; set; }
+
+        [JsonProperty("meta")]
+        public JsonDatasetSearchMetadata Metadata { get; set; }
+    }
+}
