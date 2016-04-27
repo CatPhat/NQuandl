1b89718716b819c16dfc27ff5ccdb0f8de3458b0
Kian Movasagi <kian@vedos.io>
2016-04-27 16:15:55 -0700

    add entity and mapper

---
+++ NQuandl.Npgsql/Domain/Entities/DatabaseStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NpgsqlTypes;
+using NQuandl.Npgsql.Services.Helpers;
+
+namespace NQuandl.Npgsql.Domain.Entities
+{
+    [DbTableName("database_status")]
+    public class DatabaseStatus
+    {
+        [DbColumnInfo(0, "id", NpgsqlDbType.Integer)]
+        public int Id { get; set; }
+
+        [DbColumnInfo(1, "database_id", NpgsqlDbType.Integer)]
+        public int DatabaseId { get; set; }
+
+        [DbColumnInfo(2, "database_name", NpgsqlDbType.Text)]
+        public string DatabaseName { get; set; }
+
+        [DbColumnInfo(3, "datasets_available", NpgsqlDbType.Integer)]
+        public int DatasetsAvailable { get; set; }
+
+        [DbColumnInfo(4, "datasets_downloaded", NpgsqlDbType.Integer)]
+        public int DatasetsDownloaded { get; set; }
+
+        [DbColumnInfo(5, "dataset_last_checked", NpgsqlDbType.Timestamp)]
+        public DateTime DatasetLastChecked { get; set; }
+
+        [DbColumnInfo(6, "database_marked_for_download", NpgsqlDbType.Boolean)]
+        public bool DatabaseMarkedForDownload { get; set; }
+    }
+}
---
+++ NQuandl.Npgsql/Services/Mappers/DatabaseStatusMapper.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using NQuandl.Npgsql.Api;
+using NQuandl.Npgsql.Domain.Entities;
+using NQuandl.Npgsql.Services.Extensions;
+
+namespace NQuandl.Npgsql.Services.Mappers
+{
+    public class DatabaseStatusMapper : BaseDataRecordMapper<DatabaseStatus>
+    {
+        public override DatabaseStatus ToEntity(IDataRecord record)
+        {
+            return new DatabaseStatus
+            {
+                Id = record.GetInt32(AttributeMetadata.GetColumnIndexByPropertName(nameof(DatabaseStatus.Id))),
+                DatabaseId =
+                    record.GetInt32(
+                        AttributeMetadata.GetColumnIndexByPropertName(nameof(DatabaseStatus.DatabaseId))),
+                DatabaseName =
+                    record.GetStringOrDefault(
+                        AttributeMetadata.GetColumnIndexByPropertName(nameof(DatabaseStatus.DatabaseName))),
+                DatasetsAvailable =
+                    record.GetInt32(
+                        AttributeMetadata.GetColumnIndexByPropertName(nameof(DatabaseStatus.DatasetsAvailable))),
+                DatasetsDownloaded =
+                    record.GetInt32(
+                        AttributeMetadata.GetColumnIndexByPropertName(nameof(DatabaseStatus.DatasetsDownloaded))),
+                DatasetLastChecked =
+                    record.GetDateTime(
+                        AttributeMetadata.GetColumnIndexByPropertName(nameof(DatabaseStatus.DatasetLastChecked))),
+                DatabaseMarkedForDownload =
+                    record.GetBoolean(
+                        AttributeMetadata.GetColumnIndexByPropertName(nameof(DatabaseStatus.DatabaseMarkedForDownload)))
+            };
+        }
+    }
+}
