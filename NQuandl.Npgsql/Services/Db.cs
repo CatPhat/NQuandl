b7d7c6e19cbdf25f7d0a978ff69605915e2f1a9f
Kian Movasagi <kian@vedos.io>
2016-05-10 15:05:35 -0700

    clean up

--- NQuandl.Npgsql/Services/Db.cs
+++ NQuandl.Npgsql/Services/Db.cs
@@ -74,48 +74,9 @@ namespace NQuandl.Npgsql.Services
             });
         }
 
-        public async Task BulkWriteData(BulkInsertCommand command)
-        {
-            var sqlStatement = _sql.GetBulkInsertSql(command);
-            using (var connection = new NpgsqlConnection(_configuration.ConnectionString))
-            using (var importer = connection.BeginBinaryImport(sqlStatement))
-            {
-                await command.DbDatasObservable.ForEachAsync(importData =>
-                {
-                    importer.StartRow();
-                    foreach (var bulkImportData in importData.OrderBy(x => x.ColumnIndex))
-                    {
-                        importer.Write(bulkImportData, bulkImportData.DbType);
-                    }
-                });
-                importer.Close();
-            }
-        }
+     
 
         
-        public async Task ExecuteInsertCommandAsync(InsertDataCommand command)
-        {
-            var sqlStatement = _sql.GetInsertSql(command);
-            var parameters = GetParameters(command.DbDatas);
-
-            using (var connection = new NpgsqlConnection(_configuration.ConnectionString))
-            using (var cmd = new NpgsqlCommand(sqlStatement, connection))
-            {
-                await cmd.Connection.OpenAsync();
-                cmd.Parameters.AddRange(parameters);
-                cmd.Prepare();
-                await cmd.ExecuteNonQueryAsync();
-                cmd.Connection.Close();
-            }
-        }
-
-        private NpgsqlParameter[] GetParameters(IEnumerable<DbImportData> dbDatas)
-        {
-            return dbDatas.Select(dbData => new NpgsqlParameter(dbData.ColumnName, dbData.DbType)
-            {
-                Value = dbData.Data,
-                IsNullable = dbData.IsNullable
-            }).ToArray();
-        }
+       
     }
 }
