266623874603e4889032ebeefec745471cee273f
Kian Movasagi <kian@vedos.io>
2016-05-12 16:43:41 -0700

    Add class

---
+++ tests/NQuandl.Npgsql.Tests/Integration/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NQuandl.Npgsql.Tests.Integration
+{
+    public class Bootstrapper
+    {
+        public void Bootstrap()
+        {
+            
+        }
+    }
+}
