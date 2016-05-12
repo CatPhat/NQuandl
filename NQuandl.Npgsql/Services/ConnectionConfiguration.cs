523964880e950096b46f87435b563d08431c7773
Kian Movasagi <kian@vedos.io>
2016-05-12 15:03:48 -0700

    add debug configuration

--- NQuandl.Npgsql/Services/ConnectionConfiguration.cs
+++ NQuandl.Npgsql/Services/ConnectionConfiguration.cs
@@ -13,4 +13,16 @@ namespace NQuandl.Npgsql.Services
                                           "Connection Lifetime=0;" +
                                           "COMMANDTIMEOUT=5000;";
     }
+
+    public sealed class DebugConnectionConfiguration : IConfigureConnection
+    {
+        public string ConnectionString => "Host=192.168.1.3;" +
+                                          "Username=postgres;" +
+                                          "Password=postgres;" +
+                                          "Database=debug_nquandl;" +
+                                          "MINPOOLSIZE=1;" +
+                                          "MAXPOOLSIZE=40;" +
+                                          "Connection Lifetime=0;" +
+                                          "COMMANDTIMEOUT=5000;";
+    }
 }
